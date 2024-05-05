6d73f6921deecf0be41e5dbf2236f60fde462852
jairomj <jamichahe@gmail.com>
2024-05-04 19:26:29 -0600

    comenzando el proyecto con vuetify

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
+  {
+    path: "/grid",
+    name: "grid",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/GridView.vue"),
+  },
 ];
 
 const router = createRouter({
---
+++ src/views/GridView.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="6" theme="dark">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo,
+          earum quam consequatur a asperiores sunt inventore sequi impedit
+          quaerat, velit, accusantium ducimus aliquid possimus! Vitae voluptate
+          doloremque amet maxime consequuntur
+        </p>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo,
+          earum quam consequatur a asperiores sunt inventore sequi impedit
+          quaerat, velit, accusantium ducimus aliquid possimus! Vitae voluptate
+          doloremque amet maxime consequuntur
+        </p>
+      </v-col>
+    </v-row>
+    <!-- uses the current default theme -->
+    <v-card>...</v-card>
+
+    <v-theme-provider theme="high-contrast">
+      <!-- uses the high-contrast theme -->
+      <v-card>...</v-card>
+      <v-btn>...</v-btn>
+    </v-theme-provider>
+  </v-container>
+</template>
